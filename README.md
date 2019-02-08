d625984d3a1730e3da89b436eb810c61c96d2fb3
Szabó Gábor <szabogabi@gmail.com>
2019-02-08 14:55:24 +0100

    some 3rd party test run

---
+++ README.md
@@ -0,0 +1,7 @@
+Due date calculator
+=========
+
+Homework: Calculate due date from submit date and working hours
+
+
+
--- src/calculateDueDate.js
+++ src/calculateDueDate.js
@@ -2,38 +2,53 @@
   * @desc Calculate due date from a submit date and turnaround time based on work availability hours
   * @param <Date> submitDate
   * @param <Number> turnAround
-  * @return <Date> duaDate
+  * @return <Date> duaDate or boolean false
 */
 
-function calculateDueDate (submitDate, turnAround) {
+module.exports = function (submitDate, turnAround) {
     'use strict';
-    const availabilityPerDays = [0, 8, 8, 8, 8, 8, 0];
+    const availabilityPerDays = [0, 8, 8, 8, 8, 8, 0 ];
     const workStartHour = 9;
     const workEndHour = 17;
 
-    var dueDate = submitDate;
-
-    var daysPassed = 0;
-    var hoursToAllocate = turnAround + ( submitDate.getHours() - workStartHour );
-
-    var dayOfWeek = submitDate.getDays()
-
-    while (hoursToAllocate > 0 ) {
-        hoursToAllocate = hoursToAllocate - availabilityPerDays[dayOfWeek];
-        dayOfWeek = ++dayOfWeek % 7;
-        daysPassed++;
+    function inputsAreValid(submitDateToValidate, turnaroundTimeToValidate) {
+        return true;
     }
 
-    // We have the due day
-    dueDate.setDate( dueDate.getDate() + (daysPassed-1) );
+    if ( inputsAreValid(submitDate, turnAround) ) {
+        var dueDate = submitDate;
 
-    // We have the due hour
-    dueDate.setHours( workEndHour + hoursToAllocate );
 
-    return dueDate();
+        var daysPassed = 0;
+        var hoursToAllocate = turnAround + ( submitDate.getHours() - workStartHour );
+
+        var dayOfWeek = submitDate.getDay();
+
+
+        while (hoursToAllocate > 0 ) {
+            hoursToAllocate = hoursToAllocate - availabilityPerDays[dayOfWeek];
+            dayOfWeek = ++dayOfWeek % 7;
+            daysPassed++;
+        }
+
+        console.log('Input: ' + dueDate);
+
+        // We have the due day
+        dueDate.setDate( dueDate.getDate() + (daysPassed-1) );
+
+
+        // We have the due hour
+        dueDate.setHours(parseInt( workEndHour + hoursToAllocate) );
+
+        console.log('Output: ' + dueDate);
+
+
+
+        return dueDate;
+    } else {
+        return false;
+    }
+
 
 };
 
-module.exports = {
-  calculateDueDate
-};
